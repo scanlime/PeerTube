ec6e579b9759a5e6400407ad39dc416552cf6b8f
Micah Elizabeth Scott <micah@scanlime.org>
2020-02-29 09:49:02 -0800

    Change hardcoded transcode concurrency to 4
    The new server is not quite so multicore; but we still need a value greater than 1.

--- server/initializers/constants.ts
+++ server/initializers/constants.ts
@@ -138,7 +138,7 @@ const JOB_CONCURRENCY: { [id in JobType]: number } = {
   'activitypub-http-fetcher': 1,
   'activitypub-follow': 1,
   'video-file-import': 1,
-  'video-transcoding': 6,
+  'video-transcoding': 4,
   'video-import': 1,
   'email': 5,
   'videos-views': 1,
